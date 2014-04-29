ba7fc812bfe9ea1c84838e5b097ef3bd63069224
MariaBarrett <Maria@b185.edur.sc.ku.dk>
2014-04-29 11:01:37 +0200

    almost the same..

--- Maria.py
+++ Maria.py
@@ -2,10 +2,20 @@ from __future__ import division
 import numpy as np
 import glob
 from bs4 import BeautifulSoup
+import nltk 
 
-corpuspath = glob.glob("2539/CORPUS_UTF-8/*.xml")
+corpuspath = glob.glob('2539/CORPUS_UTF-8/*.xml')
 
 def from_corpus(path):
 	for xmlfile in path:
-		currentfile = BeautifulSoup(xmlfile)
-		metadata = currentfile.find("sourceDesc")
+		opened = open(xmlfile, 'r')
+		currentfile = BeautifulSoup(opened, 'xml')
+		metadata = currentfile.find('sourceDesc')
+		metadatafeat = metadata.find_all('p')
+		persondata = currentfile.find('person')
+		personfeat = persondata.find_all('p')
+		bodyt = currentfile.find('body')
+		bodytext = bodyt.get_text()
+
+		print personfeat
+from_corpus(corpuspath)
