c7f984c2735e8584d061544a4ec6f2afa259d37d
MariaBarrett <Maria@c191.edur.sc.ku.dk>
2014-05-05 15:21:52 +0200

    word-based features

--- Maria.py
+++ Maria.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy as np
 import glob
 from bs4 import BeautifulSoup
-import nltk 
+import nltk
 import lxml
 import re
 from collections import Counter
@@ -31,13 +31,15 @@ def from_corpus(path):
 
 		bodyt = currentfile.find('body')
 		bodytext = bodyt.get_text() #like the txt files
-		temp.append(len(bodytext)) #Lex total characters
+
+		#Lexical features
+		temp.append(len(bodytext)) #Lex: total characters
 		upper = sum(x.isupper() for x in bodytext)
 		lower = sum(x.islower() for x in bodytext)
-		temp.append(lower+upper) #Lex total number of characters
-		temp.append(upper) #Lex upper case
-		temp.append(sum(x.isdigit() for x in bodytext)) #Lex number of digits
-		temp.append(bodytext.count(' ')) #Lex number of whitespace characters
+		temp.append(lower+upper) #Lex: total number of characters
+		temp.append(upper) #Lex: upper case
+		temp.append(sum(x.isdigit() for x in bodytext)) #Lex: number of digits
+		temp.append(bodytext.count(' ')) #Lex: number of whitespace characters
 		#can't find a way to count tabs
 		lettercount = Counter(bodytext.lower())
 		alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -47,12 +49,35 @@ def from_corpus(path):
 		for char in specialcharacters:
 			temp.append(lettercount[char] / len(bodytext))
 
+		#Word based features (WB)
+		tokenized = nltk.tokenize.word_tokenize(bodytext)
+		temp.append(len(tokenized)) # Word-based: number of words
+		temp.append(len([word for word in tokenized if len(word)< 4])  / len(tokenized)) # WB number of short words / number of words
+		temp.append(sum([len(word) for word in tokenized]) / len(bodytext)) # WB number of characters in words / number of characters
+		temp.append(sum([len(word) for word in tokenized]) / len (tokenized)) #WB average word length
+
+		sentences = bodyt.find_all('s')
+		sum_char = 0
+		sum_word = 0
+		for sent in sentences:
+			content = sent.contents[0]
+			sum_char += len(content)
+			sum_word += len(nltk.tokenize.word_tokenize(content))
+		temp.append(sum_char / len(sentences)) #WB average characters per sentence
+		temp.append(sum_word / len(sentences)) #WB average words per sentence
+
+		tokenizedlower = [word.lower() for word in tokenized]
+		fdist = nltk.FreqDist(tokenized)
+		vocabulary = fdist.keys()
+		temp.append(len(vocabulary)) # WB number of different words - punctuation included
+		temp.append(len([word for ]))
+		
 		print temp
+
 		paragraphs = bodyt.p
 		paragraphtext = paragraphs.get_text() #use this
 
-		sentences = bodyt.find('s')
-		sentencetext = sentences.get_text() #use this
+
 	dataset.append(temp)
 	#np.asarray(dataset)
 
