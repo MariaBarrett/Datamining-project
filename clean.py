2842d1caa1a73ef457a48271297d2f6945f6467d
MariaBarrett <Maria@Macintosh-6.local>
2014-05-13 14:17:31 +0200

    outlierdetection

--- clean.py
+++ clean.py
@@ -1,11 +1,11 @@
 import pickle
 import numpy as np
 import featuremap
-from featuremap import featuremap
+from featuremap import featuremap, metadatamap
+from collections import Counter
 
-metadata = pickle.load( open( "metadata.p", "rb" ) )
-labels = np.array([a_id[:4] for a_id in metadata[:,0]])
-traindata = pickle.load( open( "dataset.p", "rb" ) )
+meta = pickle.load( open( "metadata.p", "rb" ) )
+data = pickle.load( open( "dataset.p", "rb" ) )
 
 def normalize(traindata,testdata=False):
 	"""This function normalizes a dataset. It is possible to include
@@ -61,77 +61,34 @@ def lasses(labels,data,print_l=False):
 
 	return l_names
 
+"""
+This function takes a dataset and a max accepted number of standard deviations.
+It only looks at non-fraction features, as the variance is large among fraction features 
+If it finds a value that is further away from the mean than numberofstdev standard deviations, 
+it prints the doc id, featurename and the value for visual inspection
+"""
+def outlierdetection(dataset, metadata, numberofstdev):
+	for i in xrange(len(dataset[0])): #for every lexical and wordbased feature
+		mean = np.mean(dataset[:, i])
+		stdev = np.std(dataset[:, i])
+		for j in xrange(len(dataset)):
+			if 'frac' in featuremap[i]: #if it's a frequency feature
+				pass
+			else:
+				if dataset[j][i] < (mean - (stdev*numberofstdev)) or dataset[j][i] > (mean + (stdev*numberofstdev)):
+					print "Feature: %s Mean: %.4f(%.4f), Value: %.4f , document id: %s" %(featuremap[i], mean, stdev,  dataset[j][i], metadata[j][0])
 
-def outlierdetection(dataset, numberofstdev):
-	"""This function takes a dataset and a max acceptec number of
-	standard deviations. If it finds a value that is further away
-	from the mean than numberofstdev standard deviations, it prints
-	the featurename and the value for visual inspection"""
-
-	for i in xrange(len(dataset[0])): #for every feature
-		for datapoint in dataset:
-			if datapoint[i] < (mean[i] - (stdev[i]*numberofstdev)) or datapoint[i] > (mean[i] + (stdev[i]*numberofstdev)):
-				print featuremap.featuremap[i]
-				print datapoint[i]
-
-#outlierdetection(dataset)
-
-#remove if there are more than one author
-
-
-def get_featurenumber(feature, list='featuremap.featuremap'):
-	"""This function returns the indexname of a feature when given
-	the featurename and the list (either featuremap.metadatamap or
-	featuremap.featuremap)"""
-
-	for i in xrange(len(list)):
-		if featuremap.metadatamap[i] == featurename:
-			return i
+outlierdetection(data, meta, 5)
 
 
-def clean_from_meta(metadata, dataset, index, minval=None, maxval=None, equal=None):
-	"""This function removes datapoints if a feature of a certain index has a value that
-	is either min, max or equal to a specified value. To activate either minval, maxval or
-	equal, e.g. write minval=1 and it will clean all datapoints in both metadata and dataset
-	with values above 1.0 at the specified index c counts how many datapoints have been
-	deleted in each set."""
+#i = featuremap.index('WB_num_words')
 
-	c=0
-	for i in xrange(len(metadata)): #for every datapoint
-		if minval:
-			if metadata[i][index] < float(minval):
-				c+=1
-				metadata = np.delete(metadata, i, axis=0)  
-				dataset = np.delete(dataset, i, axis=0)  
-		if maxval:
-			if metadata[i][index] > float(maxval):
-				c+=1
-				metadata = np.delete(metadata, i, axis=0)  
-				dataset = np.delete(dataset, i, axis=0)  
-		if equal:
-			if metadata[i][index] == float(equal):
-				c+=1
-				metadata = np.delete(metadata, i, axis=0)  
-				dataset = np.delete(dataset, i, axis=0)  
-	print "For index %s, %s datapoint(s) were deleted in each dataset" %(index, c)
-	return metadata, dataset, c
+"""
+This function returns True if the passed parameter is an integer
+def is_int(var):
+	try:
+    		var= int(var)
+	except ValueError:
+    		pass  # it was a string, not an int.
 
-newmeta, newdata, deleted = clean_from_meta(meta, dataset, 1, equal=2)
-
-print newmeta, newdata
-#number of authors > 1
-
-
-def countgroups(metadata, featurename):
-	"""This function count number of different target groups.
-	It expects the metadata arrays and the dataset and name of
-	the desired metadata feature name"""
-
-	index = get_featurenumber(featurename, featuremap.metadata)
-	different_values={}
-	for datapoint in metadata:
-		if datapoint[i] in different_values:
-			different_values[datapoint[i]] +=1
-		else: 
-			different_values[datapoint] = 1
-	return different_values
+"""
---
+++ outlierdetection.py
@@ -0,0 +1,85 @@
+from __future__ import division 
+import numpy as np
+import featuremap
+
+dataset = np.array([[100,3,3,4],[2,3,5,6],[3,4,4,5]])
+
+meta = np.array([[2,3],[2,3],[3,2]])
+
+mean = np.mean(dataset, axis=0)
+stdev = np.std(dataset, axis=0)
+
+"""
+This function takes a dataset and a max acceptec number of standard deviations.
+If it finds a value that is further away from the mean than numberofstdev standard deviations, 
+it prints the featurename and the value for visual inspection
+"""
+def outlierdetection(dataset, numberofstdev):
+	for i in xrange(len(dataset[0])): #for every feature
+		for datapoint in dataset:
+			if datapoint[i] < (mean[i] - (stdev[i]*numberofstdev)) or datapoint[i] > (mean[i] + (stdev[i]*numberofstdev)):
+				print featuremap.featuremap[i]
+				print datapoint[i]
+
+
+"""
+This function returns the indexname of a feature when given the featurename and the list (either featuremap.metadatamap or featuremap.featuremap)
+"""
+def get_featureindex(feature, list='featuremap.featuremap'):
+	for i in xrange(len(list)):
+		if featuremap.metadatamap[i] == featurename:
+			return i
+
+"""
+This function removes datapoints if a feature of a certain index has a value that is either min, max or equal to a specified value. 
+To activate either minval, maxval or equal, e.g. write minval=1 and it will clean all datapoints in both metadata and dataset with values above 1.0 at the specified index 
+c counts how many datapoints have been deleted in each set. 
+"""
+
+def clean_from_metaindex(metadata, dataset, index, minval=None, maxval=None, equal=None):
+	c=0
+	for i in xrange(len(metadata)): #for every datapoint
+		if minval:
+			if metadata[i][index] < float(minval):
+				c+=1
+				metadata = np.delete(metadata, i, axis=0)  
+				dataset = np.delete(dataset, i, axis=0)  
+		if maxval:
+			if metadata[i][index] > float(maxval):
+				c+=1
+				metadata = np.delete(metadata, i, axis=0)  
+				dataset = np.delete(dataset, i, axis=0)  
+		if equal:
+			if metadata[i][index] == float(equal):
+				c+=1
+				metadata = np.delete(metadata, i, axis=0)  
+				dataset = np.delete(dataset, i, axis=0)  
+	print "For index %s, %s datapoint(s) were deleted in each dataset" %(index, c)
+	return metadata, dataset, c
+
+"""
+This function removes all datapoints with more than one author
+"""
+def max_one_author(metadata, dataset):
+	index_of_authorcount = get_featureindex('number of authors', 'featuremap.metadatamap')
+	maxoneauthor_meta, maxoneauthordata, num_removed = clean_from_metaindex(meta, dataset, index_of_authorcount, maxval=1)
+	return maxoneauthor_meta, maxoneauthordata, num_removed
+
+
+"""
+This function count number of different target groups
+It expects the metadata arrays and the dataset and name of the desired metadata feature name
+"""
+def countgroups(metadata, featurename):
+	index = get_featurenumber(featurename, featuremap.metadata)
+	different_values={}
+	for datapoint in metadata:
+		if datapoint[i] in different_values:
+			different_values[datapoint[i]] +=1
+		else: 
+			different_values[datapoint] = 1
+	return different_values
+
+
+print get_featureindex('WB_Yule')
+
