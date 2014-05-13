fd372d283ef1a50f8a4d736e517ac38b6199524e
MariaBarrett <Maria@Macintosh-6.local>
2014-05-13 14:17:51 +0200

    Merge branch 'master' of https://github.com/MariaBarrett/Datamining-project
    Conflicts:
    featuremap.pyc

--- SVM.py
+++ SVM.py
@@ -2,12 +2,15 @@ from __future__ import division
 from collections import Counter
 from featuremap import featuremap, metadatamap
 import datasplit
+import clean
 import pickle
 import matplotlib.pyplot as plt
 from sklearn import svm, grid_search
 import numpy as np
 
-y_train, X_train = datasplit.natlan_train[0], datasplit.natlan_train[1]
+y_train, X_train = datasplit.NLtrain_y, datasplit.NLtrain_X
+y_test, X_test = datasplit.NLtest_y, datasplit.NLtest_X
+
 
 parameters = {'C':[1, 3, 5, 7, 9, 11], 'gamma':[0.001, 0.01, 0.1, 1, 10, 100]}
 svr = svm.SVC()
--- datasplit.py
+++ datasplit.py
@@ -8,8 +8,9 @@ from sklearn import svm, grid_search
 import numpy as np
 
 metadata = pickle.load( open( "metadata.p", "rb" ) )
-labels = np.copy(metadata[:,-4])
 data = pickle.load( open( "dataset.p", "rb" ) )
+plusone = np.where(metadata[:,9] != "1")[0] # Get indexes for all texts written by more than one person
+data, metadata = np.delete(data, plusone, 0), np.delete(metadata, plusone, 0) # Remove all texts written by more than one person from data
 
 def natlan(metadata, data):
 
@@ -20,6 +21,64 @@ def natlan(metadata, data):
 	split = int(len(zipped)*0.80)
 	train, test = zip(*zipped[:split]), zip(*zipped[split:])
 
-	return train, test
+	return np.array(train[0]), np.array(train[1]), np.array(test[0]), np.array(test[1])
 
-natlan_train, natlan_test = natlan(metadata,data)
+def grade(metadata, data):
+
+	random.seed(448)
+	unknown = np.where(metadata[:,8]=="unknown")[0]
+	data_clean = np.delete(data, unknown, 0)
+	metadata_clean = np.delete(metadata, unknown, 0)
+	grade_map = {'M':1, 'D':0}
+	labels = np.array([grade_map[g] for g in metadata_clean[:,8]]) # 1 = M, 0 = D
+	zipped = zip(labels,data_clean)
+	random.shuffle(zipped)
+	split = int(len(zipped)*0.80)
+	train, test = zip(*zipped[:split]), zip(*zipped[split:])
+
+	return np.array(train[0]), np.array(train[1]), np.array(test[0]), np.array(test[1])
+
+def level(metadata, data):
+
+	random.seed(448)
+	unknown = np.where(metadata[:,1]=="unknown")[0]
+	data_clean = np.delete(data, unknown, 0)
+	metadata_clean = np.delete(metadata, unknown, 0)
+	
+	labels = np.array([int(l) for l in metadata_clean[:,1]]) # 1, 2, 3, 4
+	
+	zipped = zip(labels,data_clean)
+	random.shuffle(zipped)
+	split = int(len(zipped)*0.80)
+	train, test = zip(*zipped[:split]), zip(*zipped[split:])
+
+	return np.array(train[0]), np.array(train[1]), np.array(test[0]), np.array(test[1])
+
+def author(metadata, data, min_text, no_authors):
+
+	random.seed(448)
+	author_count = Counter(metadata[:,-1])
+	delete_authors = np.array([a for a in author_count.keys() if author_count[a] < min_text])
+	delete_index = np.array([i for a in delete_authors for i in np.where(metadata[:,-1]==a)[0]])
+	data_clean = np.delete(data, delete_index, 0)
+	metadata_clean = np.delete(metadata, delete_index, 0)
+
+	most_freq = [(v, k) for k, v in author_count.items()]; most_freq.sort(); most_freq.reverse()
+	mf_authors = [m[1] for m in most_freq][:no_authors]
+	mf_index = np.array([i for m in mf_authors for i in np.where(metadata_clean[:,-1]==m)[0]])
+	
+	labels = np.array([int(metadata_clean[:,-1][i]) for i in mf_index])
+	data_reduced = np.array([data_clean[i] for i in mf_index])
+
+	test_index = np.array([random.choice(np.where(labels==l)[0]) for l in set(labels)])
+	zipped = zip(labels,data_reduced)
+	test = zip(*[zipped[ti] for ti in test_index])
+	train = zip(*[i for j, i in enumerate(zipped) if j not in test_index])
+
+	return np.array(train[0]), np.array(train[1]), np.array(test[0]), np.array(test[1])
+
+
+NLtrain_y, NLtrain_X, NLtest_y, NLtest_X = natlan(metadata,data)
+GRtrain_y, GRtrain_X, GRtest_y, GRtest_X = grade(metadata,data)
+LEtrain_y, LEtrain_X, LEtest_y, LEtest_X = level(metadata,data)
+AUtrain_y, AUtrain_X, AUtest_y, AUtest_X = author(metadata,data,4,10)
