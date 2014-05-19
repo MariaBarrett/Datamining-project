580e93d184bb65c52f507a8fc29847a7d5e00cda
walkingintopeople <walkingintopeople@gmail.com>
2014-05-19 13:09:26 +0200

    updates to ngram
    …

--- ngrams.py
+++ ngrams.py
@@ -10,12 +10,15 @@ from collections import Counter
 
 data = metadata.data
 
-n = 2
+# test for n = [3,4,5]
+n = 3
+max_authors = 20
 
 did = 'id' # variable with string representation of Document ID column
 aid = 'student_id' # variable with string representation of Author ID column
 
-train, test = metadata.split_authors_pd(data)
+
+train, test = metadata.split_authors_pd(data, max_authors=max_authors)
 
 # Note that both train and test contains the same authors - the only authors that have enough documents to be part of the author split.
 authors = train[aid].unique()
@@ -25,16 +28,17 @@ profiles = []
 def get_ngrams(text, n):
 	history = []
 	length = len(text)
-	for i in range(length):
-		if i+n <= length:
-			history.append(text[i:i+n])
+	for i in range(length-n):
+		history.append(text[i:i+n])
 	return history
 
 def count_ngram(text, n=n):
 	''' takes a string and return the number of ngrams and the count for each unique ngram in the string
 	'''
 
-	ngrams = nltk.utils.ngrams(text, n)
+	#ngrams = nltk.util.ngrams(text, n)
+	ngrams = get_ngrams(text, n)
+
 	number_of_ngrams = len(text)-(n-1)
 	ngram_counter = Counter(ngrams)
 	return ngram_counter, number_of_ngrams
@@ -63,24 +67,27 @@ ng_counter, number_of_ngrams = count_ngrams(texts, n)
 
 frequencies = pd.DataFrame(ng_counter.items(), columns = ['ngram', 'freq'])
 frequencies['freq'] = frequencies['freq'] / number_of_ngrams
+frequencies = frequencies.sort('freq', ascending=False)
 
-all_ngram = list(frequencies.ngram)
+all_ngram = np.array(frequencies.ngram)
 all_freq = np.array(frequencies.freq)
-	
+
 
 
 #Build ngram-frquencies for each document
 print '### Build ngram frequencies for each document. n=', n
 
-doc_id = list(train[did])
+doc_id = list(test[did])
+texts = metadata.load_corpus_txt( doc_id )
 doc_freq = []
 doc_ngram = []
 for text in texts:
-	ngram_counter, number_of_ngrams = count_ngram(text, n)
+	ng_counter, number_of_ngrams = count_ngram(text, n)
 	freq = pd.DataFrame(ng_counter.items(), columns = ['ngram', 'freq'])
 	freq['freq'] = freq['freq'] / number_of_ngrams
+	freq = freq.sort('freq', ascending=False)
 	doc_freq.append(np.array(freq.freq))
-	doc_ngram.append(list(freq.ngram))
+	doc_ngram.append(np.array(freq.ngram))
 
 # Build author profiles
 # profiles and authors variables are interlinked. profiles[10] is the author profile (list of ngram frequencies) for authors[10], etc.
@@ -92,21 +99,20 @@ for author in authors:
 	ng_counter, number_of_ngrams = count_ngrams(a_texts, n)
 	freq = pd.DataFrame(ng_counter.items(), columns = ['ngram', 'freq'])
 	freq['freq'] = freq['freq'] / number_of_ngrams
+	freq = freq.sort('freq', ascending=False)
 	auth_freq.append(np.array(freq.freq))
-	auth_ngram.append(list(freq.ngram))
+	auth_ngram.append(np.array(freq.ngram))
 
 
 #pickle.dump( variable_name, open('file.name', 'wb'))
 #pickle.load( open('file.name', 'rb') )
 
-pickle.dump( all_freq, open('ng_all_freq.p', 'wb'))
-pickle.dump( doc_id, open('ng_doc_id.p', 'wb'))
+pickle.dump( all_freq, open('ng_all_freq'+str(n)+'.p', 'wb'))
+pickle.dump( doc_id, open('ng_doc_id'+str(n)+'.p', 'wb'))
 
-pickle.dump( doc_ngram, open('ng_doc_ngram.p', 'wb'))
-pickle.dump( doc_freq, open('ng_doc_freq.p', 'wb'))
+pickle.dump( doc_ngram, open('ng_doc_ngram'+str(n)+'.p', 'wb'))
+pickle.dump( doc_freq, open('ng_doc_freq'+str(n)+'.p', 'wb'))
 
-pickle.dump( authors, open('ng_authors.p', 'wb'))
-pickle.dump( auth_ngram, open('ng_auth_ngram.p', 'wb'))
-pickle.dump( auth_freq, open('ng_auth_freq.p', 'wb'))
-
-
+pickle.dump( authors, open('ng_authors'+str(n)+'.p', 'wb'))
+pickle.dump( auth_ngram, open('ng_auth_ngram'+str(n)+'.p', 'wb'))
+pickle.dump( auth_freq, open('ng_auth_freq'+str(n)+'.p', 'wb'))
--- ngrams_dist.py
+++ ngrams_dist.py
@@ -4,38 +4,67 @@ This file calculates distances and uses knn to find authors from ngram profiles
 profiles are loaded via pickle. To generate profiles run ngram.py
 '''
 
-import metadata, nltk, pickle, time
+import metadata, nltk, pickle, time, clean
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from collections import Counter
 
+
 data = metadata.data
+n = 3
+profile_size = 2000
 
-def normalize(ds):
-	ds_norm = (ds - ds.mean()) / (ds.max() - ds.min())
-	return ds_norm
+def normalize(data):
+	"""This function normalizes a dataset. It is possible to include
+	a test dataset to be normalized with the mean and std calculated from
+	the training dataset."""
 
-def dissimilarity(profile1, profile2):
-	''' Implements the dissimilarity measure (algorithm 1, 'Algorithm 1 Profile Dissimilarity(profile1, profile2)')
+	mean = np.mean(data, axis=0)
+	std = np.std(data, axis=0)
+
+	normalized = (data-mean)/std
+
+	return normalized
+
+
+def dissimilarity(ngram1, freq1, ngram2, freq2, ):
+	''' Implements the dissimilarity measure (algorithm 1, 'Algorithm 1 Profile Dissimilarity(freq1, freq2)')
 	from Keselj, Vlado, et al. '''
+	global profile_size
+
+	def to_float(a):
+		'''converts a single length array to float. return 0 if len != 1'''
+		if any(a):
+			return float(a)
+		else:
+			return 0
+
+	def shorten_profile(profile):
+		''' takes the profile_size amount of elements of input and returns. if profile_size is larger than amounts of elements, return profile and avoid raising out of index error
+		'''
+		global profile_size
+
+		if len(profile) > profile_size:
+			return profile[:profile_size]
+		else:
+			return profile
+
+	ngram1 = shorten_profile(ngram1)
+	freq1 = normalize(shorten_profile(freq1))
+	ngram2 = shorten_profile(ngram2)
+	freq2 = normalize(shorten_profile(freq2))
 
 	sigma = 0 #sigma is the summation variable. It's final value will be the dissimilarity score
 
-	# n-grams x contained in profile1 or profile2
-	X = list(profile1['ngram']) + list(profile2['ngram'])
+	# n-grams x contained in freq1 or freq2
+	X = list(ngram1) + list(ngram2)
 	X = list(set(X))
 
 	for x in X:
-		# let f1 and f2 be frequencies of x in profile1 and profile2 (zero if they are not included)
-		if any(profile1['ngram'] == x) == True:
-			f1 = float(profile1[profile1['ngram'] ==x].freq)
-		else:
-			f1 = 0
-		if any(profile2['ngram'] == x) == True:
-			f2 = float(profile2[profile2['ngram'] ==x].freq)
-		else:
-			f2 = 0
+		# let f1 and f2 be frequencies of x in freq1 and freq2 (zero if they are not included)
+		f1 = to_float(freq1[ngram1 == x])
+		f2 = to_float(freq2[ngram2 == x])
 		# add square of normalized differences
 		sigma += (2 * (f1-f2)/(f1+f2))**2
 	return sigma
@@ -56,18 +85,23 @@ def get_likely_author(aids):
 
 
 # Load data
+
 print "Loading data"
 start = time.clock()
-frequencies = pickle.load( open('ng_frequencies.p', 'rb'))
-doc_id = pickle.load( open('ng_doc_id.p', 'rb'))
-doc_freq = pickle.load( open('ng_doc_freq.p', 'rb'))
-authors = pickle.load( open('ng_authors.p', 'rb'))
-profiles = pickle.load( open('ng_profiles.p', 'rb'))
+
+all_freq = pickle.load(open('ng_all_freq'+str(n)+'.p', 'rb'))
+
+doc_id = pickle.load(open('ng_doc_id'+str(n)+'.p', 'rb'))
+doc_ngram = pickle.load(open('ng_doc_ngram'+str(n)+'.p', 'rb'))
+doc_freq = pickle.load(open('ng_doc_freq'+str(n)+'.p', 'rb'))
+
+authors = pickle.load(open('ng_authors'+str(n)+'.p', 'rb'))
+auth_ngram = pickle.load(open('ng_auth_ngram'+str(n)+'.p', 'rb'))
+auth_freq = pickle.load(open('ng_auth_freq'+str(n)+'.p', 'rb'))
+
 print "Loading complete in ", time.clock() - start
 
-L = 1000
-
-N = 3 # number of nearest neighbours to vote for author
+#N = 3 # number of nearest neighbours to vote for author
 
 # 3 experiments
 # distances using top overall frequencies
@@ -76,24 +110,58 @@ N = 3 # number of nearest neighbours to vote for author
 
 # measure distances using ALL ngrams
 
-nearest_author = []
-author_by_vote = []
 
-for i, d in enumerate(doc_freq):
-	d = doc_freq[0]
+def get_author_from_id(did):
+	return int(did[:-1])
+
+
+doc_predict = []
+
+for df, dn, did, i in zip(doc_freq, doc_ngram, doc_id, range(len(doc_id))):
+	#df = doc_freq[3]; dn = doc_ngram[3] #for testing purpose only!!!
 	dissimilarities = []
-	for p in profiles:
-		dissimilarities.append(dissimilarity(d, p))
+	for af, an in zip(auth_freq, auth_ngram):
+		dissimilarities.append(dissimilarity(dn, df, an, af))
 	index = np.argsort(dissimilarities) #sorts ascending (1,2,3...)
-
-	nearest_author.append(get_author(doc_id[index[0]]))
-	author_by_vote.append(get_likely_author(doc_id[index[:N]]))
-
-pickle.dump( nearest_author, open('nearest_author.p', 'wb'))
-pickle.dump( author_by_vote, open('author_by_vote.p', 'wb'))
+	predicton = authors[index[0]]
+	doc_predict.append(predicton)
+	print did, get_author_from_id(did) == predicton, index, dissimilarities[:4]
 
 
+def predict_single_doc(i):
+	global doc_freq, doc_ngram, auth_freq, auth_ngram
 
+	df = doc_freq[i]; dn = doc_ngram[i]
+	dissimilarities = []
+	for af, an in zip(auth_freq, auth_ngram):
+		dissimilarities.append(dissimilarity(dn, df, an, af))
+	index = np.argsort(dissimilarities) #sorts ascending (1,2,3...)
+	return dissimilarities, index
+
+
+
+def check_top_x_likely_author(number_of_docs, random=True):
+	global doc_id
+
+
+	if random == True:
+		this_range = range(len(doc_id))
+		random.shuffle(this_range)
+		this_range = this_range[:number_of_docs]
+	else:
+		this_range = range(number_of_docs)
+
+	# Get predictions
+	index = {}
+	for i in this_range:
+		index[i] = predict_single_doc(i)
+
+	# Display how many authors are predicted as being more likely correct than current author
+	for i in this_range:
+		a = int(doc_id[i][:-1])
+		print doc_id[i], where(index[i]==1)
+
+	return index
 
 
 
