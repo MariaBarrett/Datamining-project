25572fdc61b1a7589f4da7dff0ccba67cfc010b6
walkingintopeople <walkingintopeople@gmail.com>
2014-05-20 13:12:26 +0200

    top10 plots

--- descriptive_stats.py
+++ descriptive_stats.py
@@ -39,10 +39,13 @@ filepath ='2539/documentation/'
 filename = 'BAWE.xls'
 
 mdata = pd.read_excel(filepath+filename, 'Sheet1')
-
+# Clean data
+mdata.grade[mdata.grade==' ']= 'unknown'
+mdata.L1[mdata.L1 != 'English'] = 'Other'
 
 #----------------------------------------------------------------------------------------
 # Begin descriptive
+#    FUNCTIONS
 #----------------------------------------------------------------------------------------
 
 def build_hist_box(data, groupby='disciplinary group', columns = ['words','s-units','p-units']):
@@ -56,11 +59,57 @@ def build_hist_box(data, groupby='disciplinary group', columns = ['words','s-uni
 	plt.show()
 
 
+#----------------------------------------------------------------------------------------
+# Begin descriptive
+#----------------------------------------------------------------------------------------
+
+#build_hist_box(mdata, groupby='level')
+#build_hist_box(mdata, groupby='grade')
 
 
+# top 10 features for native language
+plt.figure()
+NL = ['SYN_frac_while', 'SYN_frac_this', 'SYN_frac_besides', 'LEX_frac_h', 'SYN_frac_since', 'SYN_frac_nevertheless', 'SYN_frac_among', 'SYN_frac_might', 'SYN_frac_whilst', 'SYN_frac_a']
+NL_index = [featuremap.index(i) for i in NL]
 
+# get all columns in NL from data where 'first language' == 'English'
+english = NLtrain_Xn[NLtrain_y == 1][:,NL_index]
+# get all columns in NL from data where 'first language' == 'Other'
+other = NLtrain_Xn[NLtrain_y == 0][:,NL_index]
 
+everything = []
+new_nl = []
+for i, t in enumerate(NL):
+	everything.append(english[:,i])
+	everything.append(other[:,i])
+	new_nl.append(t+'ENG')
+	new_nl.append(t+'OTH')
+plt.boxplot(everything)
+plt.xticks(range(20), new_nl, size='small', rotation=45)
+plt.title('top 10 features for Native Language (eng vs oth)')
 
+# top 10 features for grade
+plt.figure()
+GR = ['SYN_frac_their', 'LEX_frac_-', 'SYN_frac_POS_PRON', 'LEX_frac_(', 'LEX_frac_)', 'SYN_frac_all_funcwords', 'SYN_frac_they', 'SYN_frac_that', 'SYN_frac_POS_PRT', 'LEX_frac_word_len8']
+
+GR_index = [featuremap.index(i) for i in GR]
+
+# 1 = M, 0 = D
+# get all columns in NL from data where 'first language' == 'English'
+M = GRtrain_Xn[GRtrain_y == 1][:,GR_index]
+# get all columns in GR from data where 'first language' == 'Other'
+D = GRtrain_Xn[GRtrain_y == 0][:,GR_index]
+
+everything = []
+new_nl = []
+for i, t in enumerate(GR):
+	everything.append(english[:,i])
+	everything.append(other[:,i])
+	new_nl.append(t+'ENG')
+	new_nl.append(t+'OTH')
+plt.boxplot(everything)
+plt.xticks(range(20), new_nl, size='small', rotation=45)
+plt.title('top 10 features for Native Language (eng vs oth)')
 
 
 
